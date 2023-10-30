28d8e203354c6b92655d01543dff83a0ffcaf644
Aki Kim <akim10.96@gmail.com>
2023-10-30 14:34:17 -0400

    first commit

---
+++ browser/store/index.js
@@ -0,0 +1,31 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunks from 'redux-thunk';
+
+import minions from './minions';
+import meetings from './meetings';
+import ideas from './ideas';
+import selectedMinion from './selectedMinion';
+import selectedIdea from './selectedIdea';
+import work from './work';
+import appState from './appState';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const thunk = require('redux-thunk').default
+
+const store = createStore(
+  combineReducers({
+    minions,
+    meetings,
+    ideas,
+    selectedMinion,
+    selectedIdea,
+    work,
+    appState,
+  }),
+  composeEnhancers(
+    applyMiddleware(thunk)
+  )
+)
+
+export default store;
